4caee2a1e6b1a9bc0e2f7f088de6173136d9a414
webia1 <webia1.github@gmail.com>
2024-12-22 19:31:53 +0100

    Update .gitignore, remove obsolete .gitkeep files, and enhance LaTeX template for improved document formatting

--- Generators/quick-start-nx-angular-nest.sh
+++ Generators/quick-start-nx-angular-nest.sh
@@ -1,14 +1,15 @@
-pandoc articles/Nest.js/Quick-Start-Nx-Angular/ReadMe.md \
+pandoc markdown/Nest.js/Quick-Start-Nx-Angular/ReadMe.md \
   --template=pandoc/template.tex \
   --metadata-file=pandoc/metadata.yaml \
   --filter mermaid-filter \
   --pdf-engine=lualatex \
   --pdf-engine-opt=-shell-escape \
   --lua-filter=pandoc/graphviz-svg.lua \
+  --lua-filter=pandoc/admonution.lua \
   --toc=true \
   --toc-depth=6 \
   --highlight=kate \
   --number-sections \
   -f markdown+emoji+pipe_tables+raw_html+fenced_divs \
   --shift-heading-level-by=-1 \
-  -o articles/Nest.js/Quick-Start-Nx-Angular/pdf/Quick-Start-Nx-Angular-Nest-v0.0.8.pdf
+  -o articles/Nest.js/Quick-Start-Nx-Angular/pdf/Quick-Start-Nx-Angular-Nest-v0.0.12.pdf
